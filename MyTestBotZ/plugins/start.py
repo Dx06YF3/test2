19346d3981a8d2d2a7b187c39b11097c7410a768
Dx06YF3 <92134301+Dx06YF3@users.noreply.github.com>
2022-01-22 19:52:02 +0530

    update start.py

--- MyTestBotZ/plugins/start.py
+++ MyTestBotZ/plugins/start.py
@@ -1,5 +1,16 @@
 from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+
+
 @Client.on_message(Filters.command(["start"]), group=-2)
 async def start(client, message):
+    # return
+    joinButton = InlineKeyboardMarkup([
+        [InlineKeyboardButton("Channel", url="https://t.me/"), InlineKeyboardButton("Creator", url="https://telegram.me/n") ],
+        [InlineKeyboardButton(
+            "Source Code", url="https://github.com/")]
+    ])
+    welcomed = f"""Hey <b>{message.from_user.first_name}</b>\nA Simple YouTube Downloader Bot that can:
+  ➠ Download YouTube videos
+  ➠ Download audio from YouTube videos \n\n Made with"""
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
